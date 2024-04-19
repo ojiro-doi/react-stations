395da7575f868b718f994ff04e1e8c16f6db3643
ojiro-doi <sakura.0046@icloud.com>
2024-04-19 12:18:13 +0900

    station4~6クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 // DO NOT DELETE
 
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/terrier-tibetan/n02097474_2895.jpg');
+
+  const clickButton = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setDogUrl(data.message);
+      })
+      .catch((error) => {
+        console.log('失敗しました');
+      });
+  };
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
+      <main>
+        <h3>犬の画像を表示するサイトです</h3>
+        <div>
+          <img src={dogUrl} alt="" />
+          <button onClick={() => setDogUrl(clickButton)}>更新</button>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
